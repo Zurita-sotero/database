67a288ed848ea0e28bc23768fde31b60073f7cba
Sotero Zurita <isc20350328@gmail.com>
2023-10-09 13:42:42 -0600

    Actualizacion

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,21 @@
 const {request, response} = require('express');
-
+const conn=require('../db');
 
 const listUsers = (req = request, res = response) => {
-    res.json({msg: "Holaa Usuario, llevame con tu lider..."})
+    try{
+    conn.query ('SELECT * FROM users', (err,result)=>{
+        if(err){
+            throw err
+        }
+        res.json(result);
+    });
+    }catch (error){
+        res.status(500).json(error);
+    }finally{
+        conn.end();
+    }
+    
 }
+   
 
-module.exports={listUsers}
+module.exports={listUsers};
--- db.js
+++ db.js
@@ -1,19 +1,22 @@
 const mysql = require('mysql');
 
-const connection = {
-    host:'localhost',
-    port:3306,
-    database:'backend',
-    user:'root',
-    password: ''
+const config = {
+    host: 'localhost',
+    port: 3306,
+    database: 'backend',
+    user: 'root',
+    password:''
+    
 };
 
-const conn = mysql.createConnection(connection);
+const conn = mysql.createConnection(config);
 
-conn.connect((err) => {
-    if(err){
-        console.log("Error ocurred while connecting to MySQL database.")
+conn.connect((err) =>{
+    if (err) {
+        console.log("ERROR OCURRED WHILE CONNECTING TO MYSQL DATABASSE.", err);
     } else {
-        console.log("Connection with MySQL database created successfully.")
+        console.log("CONNECTION WITH MYSQL DATABASE CREATED SUCCESSFULLY.");
     }
-})
+});
+
+module.exports = conn;
