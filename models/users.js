0413c697e624226f6e05800afa59580fa6f3a949
Sotero Zurita <isc20350328@gmail.com>
2023-10-19 13:07:34 -0600

    Deleet and Update

--- models/users.js
+++ models/users.js
@@ -1,49 +1,77 @@
-const usermodels = {
-    getAll: `
-    SELECT 
-    * 
-    FROM 
-    Users`,
-getByID:`
-    SELECT
-    *
-    FROM
-     Users
-    WHERE
-      id = ?
-`,
-addRow: `
-    INSERT INTO
-    Users (
-     username,
-     email,
-     password,
-     name,
-     lastname,
-     phone_number,
-     role_id,
-     is_active
-    ) 
-VALUES (?, ?, ?, ?, ?, ?, ?, ?)
-`,
+const usersModel = {
+    getAll:`
+            SELECT 
+                * 
+            FROM 
+                Users`,
 
-getByUsername: `
-     SELECT 
-         id 
-     FROM
-         Users
-    WHERE
-         username= ? 
-`,
+    getByID : `
+            SELECT
+                *
+            FROM
+                Users
+            WHERE
+                id = ?
+            `,
+    
+    addRow: `
+            INSERT INTO
+                Users (
+                    username,
+                    email,
+                    password,
+                    name,
+                    lastname,
+                    phone_number,
+                    role_id,
+                    is_active
+                ) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+    `,
+    
+    getByUsername: `
+            SELECT 
+                id 
+            FROM
+                Users
+            WHERE
+                username = ?
+    `,
 
-getByEmail:`
-    SELECT 
-         id 
-    FROM
-        Users
-     WHERE
-         email= ? 
+    getByEmail: `
+            SELECT 
+                id
+            FROM
+                Users
+            WHERE
+                email = ?
+    `, 
+
+    updateUser: `
+         UPDATE Users
+         SET 
+             username = ?,
+             email = ?,
+             password = ?,
+             name = ?,
+             lastname = ?,
+             phone_number = ?,
+             is_active = ?
+         WHERE
+             id = ?
+  `,
+updateRow: `
+         PENDIENTE
+
 `,
+deleteRow:`
+        UPDATE
+            Users
+            SET
+            is_active = 0
+            WHERE
+            ID =?
+`
 }
 
-module.exports = usermodels;
+module.exports = usersModel;
